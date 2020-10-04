08a197db3b5ae4f4c2d36fdc52a39b40be19faa6
Brandon Ramirez <bran.ramirez.don@gmail.com>
2020-10-04 00:09:34 -0700

    Implemented some basic login form components from ant design

--- src/components/pages/LandingPage/LandingPage.js
+++ src/components/pages/LandingPage/LandingPage.js
@@ -1,10 +1,45 @@
 import React from 'react';
 import StyledLandingPage from './StyledLandingPage';
 
+import { Button, Checkbox, Form, Input } from 'antd';
+import { UserOutlined, LockOutlined } from '@ant-design/icons';
+import { Link } from 'react-router-dom';
+
 const LandingPage = (props) => {
   return (
     <StyledLandingPage>
       <h1>some text</h1>
+      <Form
+        initialValues={{
+          remember: true,
+        }}
+      >
+        <Form.Item name="Username">
+          <Input
+            prefix={<UserOutlined className="site-form-item-icon" />}
+            placeholder="Username"
+          />
+        </Form.Item>
+        <Form.Item name="Password">
+          <Input
+            type="password"
+            prefix={<LockOutlined className="site-form-item-icon" />}
+            placeholder="Password"
+          />
+        </Form.Item>
+        <Form.Item>
+          <Form.Item name="remember">
+            <Checkbox>Remember Me</Checkbox>
+          </Form.Item>
+
+          <Link>Register</Link>
+        </Form.Item>
+        <Form.Item>
+          <Button type="primary" htmlType="submit">
+            Log In
+          </Button>
+        </Form.Item>
+      </Form>
     </StyledLandingPage>
   );
 };
